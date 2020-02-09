5bf5b262e275a9b55a3f3b37697ca1e7b5a3dc1d
Shaun Smith <Shaun@Shauns-Air.attlocal.net>
2020-02-09 16:40:21 -0500

    learning

---
+++ platform/minilogue-xd/osc/tests/silent/silent.cpp
@@ -0,0 +1,26 @@
+/*
+ * File: silent.cpp
+ *
+ * Naive silent oscillator test
+ *
+ */
+
+#include "userosc.h"
+
+void OSC_INIT(uint32_t platform, uint32_t api)
+{
+  (void)platform;
+  (void)api;
+}
+
+void OSC_CYCLE(const user_osc_param_t * const params,
+               int32_t *yn,
+               const uint32_t frames)
+{
+  q31_t * __restrict y = (q31_t *)yn;
+  const q31_t * y_e = y + frames;
+  
+  for (; y != y_e; ) {
+    *(y++) = 0;
+  }
+}
